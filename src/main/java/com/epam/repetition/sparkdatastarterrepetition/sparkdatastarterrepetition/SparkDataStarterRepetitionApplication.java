d1e7684225c078e0661c75fc78c338f9fc51dc0b
Yauheni Savitski <yauheni.savitski@ext.conrad.com>
2021-09-18 23:41:35 +0200

    init commit

---
+++ src/main/java/com/epam/repetition/sparkdatastarterrepetition/sparkdatastarterrepetition/SparkDataStarterRepetitionApplication.java
@@ -0,0 +1,13 @@
+package com.epam.repetition.sparkdatastarterrepetition.sparkdatastarterrepetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkDataStarterRepetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkDataStarterRepetitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/repetition/sparkdatastarterrepetition/sparkdatastarterrepetition/SparkDataStarterRepetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.repetition.sparkdatastarterrepetition.sparkdatastarterrepetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkDataStarterRepetitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
